b34948bbfcbb1dbf45dcf59ab90e26aa89039cde
Mary White <mary.a.white28@gmail.com>
2021-10-23 22:10:06 -0400

    working on js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 (function(){
     "use strict"
 
+$(".inner-divs > div").click(function(){
+    var userInput = ($(this).text());
+    console.log(userInput);
+    if (userInput === 0 || userInput === "CLR") {
+        userInput = "";
+    }
+    $("#userinput").append(userInput);
+});
+
+$()
+
 })();
 
