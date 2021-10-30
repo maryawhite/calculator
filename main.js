0896b060f65d4db00b3c64ba9337556e2178a521
Mary White <mary.a.white28@gmail.com>
2021-10-30 16:05:34 -0400

    completed basic functionality. need to fix font size on output, and account for dividing by zero maybe

--- main.js
+++ main.js
@@ -52,27 +52,49 @@
         $("#output").text(divideby100);
     });
 
-    //when you click on the +/- sign, it should convert a positive number to negative and a negative number to positive
-    $("#pos-neg").click(function(){
-        var userInput = parseInt($("#userinput").text());
-        console.log(userInput);
-        if(userInput > 0) {
-            userInput *= -1;
-        }
-        console.log(userInput);
-        return userInput;
-    });
-//if the userinput is positive, change it to negative and if it's negative, change it to positive...
-    //this is converting a postive num to neg, but it's not saving it or evaluating it as negative.
 
-
-
-    //when they click x squared, it should eval that number ** 2. take their input and
+    //when they click x squared, it should eval that number ** 2. take their input and ** 2
     $("#xSquared").click(function(){
-        var userInput = $("#userinput").text();
+        var userInput = parseInt($("#userinput").text());
         var squared = userInput**2;
         $("#output").text(squared);
+        // $("#output").text(eval(userInput**2)) //this also works
     });
+
+    //when they click square root button, it should eval that number ** .5 (sqrt is raise to the 1/2 power)
+    $("#sqrt").click(function(){
+        var userInput = parseInt($("#userinput").text());
+        var sqrt = userInput**(1/2);
+        $("#output").text(sqrt);
+    });
+
+    $("#comet").click(function(){
+        $("body").css("background-image", "linear-gradient(315deg, #FAD961 11%, #ff8c4a 50%, #feff10 91%)");
+        $(".inner-divs .oper").css("background-color", "purple");
+        $("#paren1, #paren2").css("background-color", "#EF7BE9FF");
+    });
+
+    //when you click on the +/- sign, it should store that number and then convert it; a positive number to negative and a negative number to positive
+    // $("#pos-neg").click(function(){
+    //     var userInput = parseInt($("#userinput").text());
+    //     console.log(userInput);
+    //     if(userInput > 0) {
+    //         var newUserInput;
+    //         newUserInput = userInput * -1;
+    //     }
+    //     // console.log(userInput);
+    //     // return userInput;
+    //     $("#userinput").text(newUserInput);
+    // });
+//if the userinput is positive, change it to negative and if it's negative, change it to positive...
+
+    // OR...just append a negative sign? this only prepends it to the first number. How to do -3 + -3 = -6?
+    // $("#pos-neg").click(function(){
+    //     var userInput = parseInt($("#userinput").text());
+    //     if(userInput > 0) {
+    //         $("#userinput").prepend("-");
+    //     }
+    // });
 
 
 })();
